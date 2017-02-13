49fe2c2e7a0951b03324279f10bd0e7f0552827e
Jeremy Canning <jscanning@cpp.edu>
2017-02-12 18:20:36 -0800

    HomeWork 3: revision 1 actual: ListInterface complete, awaiting implementation for vector

---
+++ src/hwk3/VectorList.java
@@ -0,0 +1,71 @@
+package hwk3;
+
+public class VectorList<T> implements ListInterface<T> {
+
+	@Override
+	public void add(T newItem) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void add(T newItem, int location) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean remove(T item) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public T remove(int location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public T replace(int location, T newItem) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T look(int location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T[] lookAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean contains(T anItem) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
